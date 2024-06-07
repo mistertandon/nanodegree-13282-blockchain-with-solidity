c83080223b4159c1abc096900763fd1639b4ea23
mistertandon <enggparveshtandon@gmail.com>
2024-06-07 23:25:41 +0530

    feat: Modify PromVoting smart contract

--- lesson-2-solidity-design-patterns-and-dapp-interactions/demos/use-time/starter/contracts/PromVoting.sol
+++ lesson-2-solidity-design-patterns-and-dapp-interactions/demos/use-time/starter/contracts/PromVoting.sol
@@ -9,14 +9,29 @@ contract PromVoting {
         uint voteCount;
     }
 
+    struct Vote{
+        uint candidateId;
+        uint timestamp;
+    }
+
+    uint public startTime;
+
+    uint public endTime;
+
     // Mapping from candidate ID to Candidate struct for storing candidates
     mapping(uint => Candidate) public candidates;
     // Counter for the total number of candidates
     uint public candidatesCount;
 
-    // Event to be emitted when a vote is cast
-    event VoteCast(uint candidateId);
+    uint public totalCount;
 
+    mapping(address => Vote) votes;
+    // Event to be emitted when a vote is cast
+    event VoteCast(uint candidateId, uint timestamp);
+    constructor(uint _votingDuration){
+        startTime = block.timestamp;
+        endTime = startTime + _votingDuration; 
+    }
     // Function to add a new candidate to the election
     function addCandidate(string memory _name) public {
         candidatesCount += 1;
@@ -26,11 +41,14 @@ contract PromVoting {
     // Function to vote for a candidate
     function vote(uint _candidateId) public {
         require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate ID");
+        require(block.timestamp >= startTime && block.timestamp <= endTime, "Voting window has been closed.");
         // Increment the candidate's vote count
         candidates[_candidateId].voteCount += 1;
+        votes[msg.sender] = Vote(_candidateId, block.timestamp);
+        totalCount += 1;
 
         // Emit a vote event
-        emit VoteCast(_candidateId);
+        emit VoteCast(_candidateId, block.timestamp);
     }
 
     // Function to get the vote count for a specific candidate
